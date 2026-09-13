1671878c00466687e6aec4c4255f17a64ed8c418
Addy456 <paladitya456@gmail.com>
2026-09-13 10:44:02 +0530

    CSLL insertion complete

--- LL/CircularSinglyLinkedList.java
+++ LL/CircularSinglyLinkedList.java
@@ -1,3 +1,5 @@
+import java.net.SocketPermission;
+
 public class CircularSinglyLinkedList{
     
     static class Node{
@@ -39,11 +41,13 @@ public class CircularSinglyLinkedList{
         }
 
         //case 2: Non Empty CSLL
-        newNode.next = head;
-        head = newNode;
+        else{
+            newNode.next = head;
+            head = newNode;
 
-        // circular connection
-        tail.next = head;
+            // circular connection
+            tail.next = head;
+        }
 
         // update size
         size++;
@@ -61,12 +65,15 @@ public class CircularSinglyLinkedList{
             // circular connection
             tail.next = head;
         }
-        // case 2: Non empty CSLL
-        tail.next = newNode;
-        newNode = tail;
 
-        // circular connection
-        tail.next = head;
+        // case 2: Non empty CSLL
+        else{
+            tail.next = newNode;
+            tail = newNode;
+
+            // circular connection
+            tail.next = head;
+        }
 
         // update size
         size++;
@@ -76,7 +83,7 @@ public class CircularSinglyLinkedList{
     public void insertAtPosition(int position, int data){
 
         // case 1: positions are invalid
-        if(position < 1 || position > size ){
+        if(position < 1 || position > size + 1){
             System.out.println("position is invalid");
             return;
         }
@@ -84,17 +91,84 @@ public class CircularSinglyLinkedList{
         // case 2: position is 1
         if(position == 1){
             insertAtHead(data);
+            return;
         }
 
         // case 3: position is size
-        if(position == size){
+        if(position == size + 1){
             insertAtTail(data);
+            return;
         }
 
         // case 4: position is in middle
+            Node prevNode = head;
+
+        for(int i=1; i <= position-2; i++){
+            prevNode = prevNode.next;
+        }
+
+        Node nextNode = prevNode.next;
+
+        Node newNode = new Node(data);
+
+        prevNode.next = newNode;
+        newNode.next = nextNode;
+
+        // update size
+        size++;   
+    }
+
+    //=====================================
+    //Print circular singly linked list
+    //=====================================
+
+    public void printList(){
+        // case 1: Empty CSLL
+        if(head == null){
+            System.out.println("Empty linked list");
+            return;
+        }
+
         Node temp = head;
 
-        for
+        //printing CSLL
+        do{
+            System.out.print(temp+"->");
+            temp = temp.next;
+        }while(temp!=head);
 
+        System.out.println("Back to Head");
+    }
+
+    //========================
+    //Searching
+    //========================
+    
+    public boolean search(int target){
+        
+        if(head == null){
+            return false;
+        }
+
+        Node temp = head;
+
+        do{
+            if(temp.data == target){
+                return true;
+            }
+            else{
+                temp = temp.next;
+            }
+        }while(temp!=head);
+        
+        return false;
+    }
+
+    public static void main(String[] args) {
+        CircularSinglyLinkedList mylist = new CircularSinglyLinkedList();
+
+        mylist.insertAtHead(10);
+        mylist.printList();
+        
     }
 }
