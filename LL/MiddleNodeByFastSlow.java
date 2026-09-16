cc0de9eb0dafb244a68a7cc1bd8a4b9231064a14
Addy456 <paladitya456@gmail.com>
2026-09-16 21:00:54 +0530

    update in main

--- LL/MiddleNodeByFastSlow.java
+++ LL/MiddleNodeByFastSlow.java
@@ -20,5 +20,37 @@ public class MiddleNodeByFastSlow {
         return slow;
     }
 
-    public static void main(String[] args) {}
+     public void printList(Node head){
+        Node temp = head;
+        while(temp != null){
+            System.out.print(temp.val+"->");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        MiddleNodeByFastSlow obj = new MiddleNodeByFastSlow();
+
+        Node n1 = obj.new Node(10);
+        Node n2 = obj.new Node(20);
+        Node n3 = obj.new Node(30);
+        Node n4 = obj.new Node(40);
+        Node n5 = obj.new Node(50);
+        Node n6 = obj.new Node(50);
+
+
+        n1.next = n2;
+        n2.next = n3;
+        n3.next = n4;
+        n4.next = n5;
+        n5.next = n6;
+
+
+        System.out.println("original list");
+        obj.printList(n1);     
+
+        System.out.println(obj.mid(n1).val);
+    
+    }
 }
