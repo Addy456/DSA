fb8843bfed767d6abf33fb7f7bca8115741ba342
Addy456 <paladitya456@gmail.com>
2026-09-16 21:04:55 +0530

    update in main

--- LL/ReverseListByIteration.java
+++ LL/ReverseListByIteration.java
@@ -19,5 +19,35 @@ class ReverseListByIteration {
         }
         return prev;
     }    
-    public static void main(String[] args){}
+
+    public void printList(Node head){
+        Node temp = head;
+        while(temp != null){
+            System.out.print(temp.val+"->");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+    public static void main(String[] args){
+        ReverseListByIteration obj = new ReverseListByIteration();
+
+        Node n1 = obj.new Node(10);
+        Node n2 = obj.new Node(20);
+        Node n3 = obj.new Node(30);
+        Node n4 = obj.new Node(40);
+        Node n5 = obj.new Node(50);
+
+        n1.next = n2;
+        n2.next = n3;
+        n3.next = n4;
+        n4.next = n5;
+
+        System.out.println("original list");
+        obj.printList(n1);
+
+        
+
+        System.out.println("reversed list");
+        obj.printList(obj.reverseLinkedList(n1));
+    }
 }
