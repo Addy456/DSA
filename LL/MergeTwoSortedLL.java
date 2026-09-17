a410455d8b1cc0953e35702bca49d9862071fd96
Addy456 <paladitya456@gmail.com>
2026-09-17 07:20:38 +0530

    using merge 2 sorted ll

--- LL/MergeTwoSortedLL.java
+++ LL/MergeTwoSortedLL.java
@@ -45,7 +45,36 @@ public class MergeTwoSortedLL {
         System.out.println();
     }
     public static void main(String[] args){
-       
-    }
+        MergeTwoSortedLL obj = new MergeTwoSortedLL();
 
+        Node n1 = obj.new Node(10);
+        Node n2 = obj.new Node(20);
+        Node n3 = obj.new Node(30);
+        Node n4 = obj.new Node(40);
+        Node n5 = obj.new Node(50);
+
+        n1.next = n2;
+        n2.next = n3;
+        n3.next = n4;
+        n4.next = n5;
+
+        Node m1 = obj.new Node(15);
+        Node m2 = obj.new Node(25);
+        Node m3 = obj.new Node(35);
+        Node m4 = obj.new Node(45);
+
+        m1.next = m2;
+        m2.next = m3;
+        m3.next = m4;
+
+        System.out.println("original list 1 :");
+        obj.printList(n1);
+
+        System.out.println("original list 2 :");
+        obj.printList(m1);
+        
+
+        System.out.println("merge list :");
+        obj.printList(obj.merge(n1,m1));
+    }
 }
